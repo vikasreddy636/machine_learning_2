03a505647621466989ca4cc6c978e234918a1027
vikasreddy636 <vikasreddy636@rediffmail.com>
2022-03-30 15:07:33 +0200

    classifaication updated

--- classification.py
+++ classification.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn as sk
 from sklearn import *
@@ -18,26 +17,57 @@ from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
-
 import warnings
-warnings.filterwarnings("ignore")
+import math
 
+warnings.filterwarnings("ignore")
 
 x = pd.read_csv(r'LNIP_super_all.csv')
 
+def sensitivity(tn,fp,fn,tp):
+    return round(float(tp/(tp+fn)),2)
+
+def specificity(tn,fp,fn,tp):
+    return round(float(tn/(tn+fp)),2)
+
+def precision(tn,fp,fn,tp):
+    return round(float(tp/tn+fp),2)
+
+def error_rate(tn,fp,fn,tp):
+    return round((fp+fn)/(tp+tn+fp+fn),2)
+
+def quality_index(sensitivity,specificity):
+    return round(float(math.sqrt(sensitivity*specificity)),2)
+
 training_set,test_set = train_test_split(x,test_size=0.2,random_state=0)
-X_train = training_set.iloc[:,0:59].values
-Y_train = training_set.iloc[:,59].values
-X_test = test_set.iloc[:,0:59].values
-Y_test = test_set.iloc[:,59].values
+X_train = training_set.iloc[:,0:512].values
+Y_train = training_set.iloc[:,512].values
+X_test = test_set.iloc[:,0:512].values
+Y_test = test_set.iloc[:,512].values
 
 #################MLP Classifier#################
 classifier = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
 classifier.fit(X_train, Y_train)
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_pred, Y_test)
+tn = cm[0][0]
+fp = cm[0][1]
+fn = cm[1][0]
+tp = cm[1][1]
+sens1 = sensitivity(tn,fp,fn,tp)
+spec1 = specificity(tn,fp,fn,tp)
+prec  = precision(tn,fp,fn,tp)
+err_rate = error_rate(tn,fp,fn,tp)
+QI = quality_index(sens1,spec1)
+print("################ Details of MLP Classifier ########################")
+print("Sensitivity -> "+str(sens1))
+print("Specificity -> "+str(spec1))
+print("Precision   -> "+str(prec))
+print("Error Rate  -> "+str(err_rate))
+print("Quality Index-> "+str(QI))
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("Accuracy of MLP classifier: ",accuracy*100)
+print("Accuracy : ",accuracy*100)
+print("\n")
 
 #################Optimized SVM with RBF Kernel###############
 #For getting 98% accuracy feed C=1100 and Gamma = 1000
@@ -49,32 +79,77 @@ classifier = SVC(C=1100, cache_size=200, class_weight=None, coef0=0.0,
 classifier.fit(X_train,Y_train)
 SVM_pred = classifier.predict(X_test)
 cm = confusion_matrix(Y_test,SVM_pred)
+tn = cm[0][0]
+fp = cm[0][1]
+fn = cm[1][0]
+tp = cm[1][1]
+sens1 = sensitivity(tn,fp,fn,tp)
+spec1 = specificity(tn,fp,fn,tp)
+prec  = precision(tn,fp,fn,tp)
+err_rate = error_rate(tn,fp,fn,tp)
+QI = quality_index(sens1,spec1)
+print("################ Details of Optimized RBF kernel ########################")
+print("Sensitivity -> "+str(sens1))
+print("Specificity -> "+str(spec1))
+print("Precision   -> "+str(prec))
+print("Error Rate  -> "+str(err_rate))
+print("Quality Index-> "+str(QI))
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of Optimized SVM with RBF kernel "+str(accuracy*100))#+" by optimized RBF with 'C'= "+str(1100)+" Gamma value= "+str(1000))
+print("Accuracy : ",accuracy*100)
+print("\n")
+##print(cm)
+##accuracy = float(cm.diagonal().sum())/len(Y_test)
+##print("\nAccuracy Of Optimized SVM with RBF kernel "+str(accuracy*100))#+" by optimized RBF with 'C'= "+str(1100)+" Gamma value= "+str(1000))
 
 ########################Linear SVM Classifier###############
 SVM = svm.LinearSVC()
 SVM.fit(X_train,Y_train)
 SVM_pred = SVM.predict(X_test)
 cm = confusion_matrix(Y_test,SVM_pred)
+tn = cm[0][0]
+fp = cm[0][1]
+fn = cm[1][0]
+tp = cm[1][1]
+sens1 = sensitivity(tn,fp,fn,tp)
+spec1 = specificity(tn,fp,fn,tp)
+prec  = precision(tn,fp,fn,tp)
+err_rate = error_rate(tn,fp,fn,tp)
+QI = quality_index(sens1,spec1)
+print("################ Details of Linear SVM ########################")
+print("Sensitivity -> "+str(sens1))
+print("Specificity -> "+str(spec1))
+print("Precision   -> "+str(prec))
+print("Error Rate  -> "+str(err_rate))
+print("Quality Index-> "+str(QI))
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of Linear SVM For The Given Dataset: ", accuracy*100)
-
-##############################Naive Bayes################
-gnb = GaussianNB() 
-gnb.fit(X_train, Y_train) 
-y_pred = gnb.predict(X_test) 
-cm = confusion_matrix(Y_test,y_pred)
-accuracy = float(cm.diagonal().sum())/len(Y_test)
-#print("\nAccuracy of Naive Bayes Classsifier:", accuracy*100)
+print("Accuracy : ",accuracy*100)
+print("\n")
+##accuracy = float(cm.diagonal().sum())/len(Y_test)
+##print("\nAccuracy Of Linear SVM For The Given Dataset: ", accuracy*100)
 
 ######################## Random Forest Classifier#################
 rfc = RandomForestClassifier()
 rfc.fit(X_train,Y_train)
 rfc_predict = rfc.predict(X_test)
 cm = confusion_matrix(Y_test,rfc_predict)
+tn = cm[0][0]
+fp = cm[0][1]
+fn = cm[1][0]
+tp = cm[1][1]
+sens1 = sensitivity(tn,fp,fn,tp)
+spec1 = specificity(tn,fp,fn,tp)
+prec  = precision(tn,fp,fn,tp)
+err_rate = error_rate(tn,fp,fn,tp)
+QI = quality_index(sens1,spec1)
+print("################ Details of Random Forest ########################")
+print("Sensitivity -> "+str(sens1))
+print("Specificity -> "+str(spec1))
+print("Precision   -> "+str(prec))
+print("Error Rate  -> "+str(err_rate))
+print("Quality Index-> "+str(QI))
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of Random Forest For The Given Dataset :", accuracy*100)
+print("Accuracy : ",accuracy*100)
+print("\n")
 
 ###################### XG Boost############################
 model = XGBClassifier()
@@ -82,19 +157,46 @@ model.fit(X_train, Y_train)
 #make predictions for test data
 y_pred = model.predict(X_test)
 cm = confusion_matrix(Y_test,y_pred)
-accuracy = float(cm.diagonal().sum())/len(Y_test)
-predictions = [round(value) for value in y_pred]
+tn = cm[0][0]
+fp = cm[0][1]
+fn = cm[1][0]
+tp = cm[1][1]
+sens1 = sensitivity(tn,fp,fn,tp)
+spec1 = specificity(tn,fp,fn,tp)
+prec  = precision(tn,fp,fn,tp)
+err_rate = error_rate(tn,fp,fn,tp)
+QI = quality_index(sens1,spec1)
 #evaluate predictions
-accuracy = accuracy_score(Y_test, predictions)
-print("\nAccuracy of XGBoost: %.2f%%" % (accuracy * 100.0))
+print("################ Details of XG Boost ########################")
+print("Sensitivity -> "+str(sens1))
+print("Specificity -> "+str(spec1))
+print("Precision   -> "+str(prec))
+print("Error Rate  -> "+str(err_rate))
+print("Quality Index-> "+str(QI))
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+print("Accuracy : ",accuracy*100)
+print("\n")
 
 ###################### Naive Bayes ########################
 gnb = GaussianNB()
 gnb.fit(X_train, Y_train)
 y_pred = gnb.predict(X_test)
 cm = confusion_matrix(Y_test,y_pred)
+tn = cm[0][0]
+fp = cm[0][1]
+fn = cm[1][0]
+tp = cm[1][1]
+sens1 = sensitivity(tn,fp,fn,tp)
+spec1 = specificity(tn,fp,fn,tp)
+prec  = precision(tn,fp,fn,tp)
+err_rate = error_rate(tn,fp,fn,tp)
+QI = quality_index(sens1,spec1)
+print("################ Details of Naive Bayes ########################")
+print("Sensitivity -> "+str(sens1))
+print("Specificity -> "+str(spec1))
+print("Precision   -> "+str(prec))
+print("Error Rate  -> "+str(err_rate))
+print("Quality Index-> "+str(QI))
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-predictions = [round(value) for value in y_pred]
-#evaluate predictions
-accuracy = accuracy_score(Y_test, predictions)
-print("\nAccuracy of Naive Bayes: %.2f%%" % (accuracy * 100.0))
+print("Accuracy : ",accuracy*100)
+print("\n")
