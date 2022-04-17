bace7784036357990973daaedf08bf2870941746
Ginne vikas Reddy <73032093+vikasreddy636@users.noreply.github.com>
2022-04-17 18:11:31 +0000

    final updates done

--- classification.py
+++ classification.py
@@ -46,6 +46,12 @@ X_test = test_set.iloc[:,0:512].values
 Y_test = test_set.iloc[:,512].values
 
 #################MLP Classifier#################
+#hidden_layer_sizes : This parameter allows us to set the number of layers and the number of nodes we wish to have in the Neural Network Classifier.
+#Each element in the tuple represents the number of nodes at the ith position where i is the index of the tuple. Thus the length of tuple denotes the total number of hidden layers in the network.
+#max_iter: It denotes the number of epochs.
+#activation: The activation function for the hidden layers.
+#solver: This parameter specifies the algorithm for weight optimization across the nodes.
+#random_state: The parameter allows to set a seed for reproducing the same results
 classifier = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
 classifier.fit(X_train, Y_train)
 y_pred = classifier.predict(X_test)
@@ -71,7 +77,7 @@ print("\n")
 
 #################Optimized SVM with RBF Kernel###############
 #For getting 98% accuracy feed C=1100 and Gamma = 1000
-
+#         https://scikit-learn.org/0.18/modules/generated/sklearn.svm.NuSVC.html
 classifier = SVC(C=1100, cache_size=200, class_weight=None, coef0=0.0,
     decision_function_shape='ovr', degree=3, gamma=1000, kernel='rbf', max_iter=-1,
     probability=False, random_state=None, shrinking=True, tol=0.001,
@@ -102,8 +108,11 @@ print("\n")
 ##print("\nAccuracy Of Optimized SVM with RBF kernel "+str(accuracy*100))#+" by optimized RBF with 'C'= "+str(1100)+" Gamma value= "+str(1000))
 
 ########################Linear SVM Classifier###############
+#       https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html
 SVM = svm.LinearSVC()
+#   Fit the model according to the given training data.
 SVM.fit(X_train,Y_train)
+#   Predict class labels for samples in X.
 SVM_pred = SVM.predict(X_test)
 cm = confusion_matrix(Y_test,SVM_pred)
 tn = cm[0][0]
@@ -128,8 +137,11 @@ print("\n")
 ##print("\nAccuracy Of Linear SVM For The Given Dataset: ", accuracy*100)
 
 ######################## Random Forest Classifier#################
+#       https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
 rfc = RandomForestClassifier()
+#	 Build a forest of trees from the training set (X, Y).
 rfc.fit(X_train,Y_train)
+#	 Predict class for X.
 rfc_predict = rfc.predict(X_test)
 cm = confusion_matrix(Y_test,rfc_predict)
 tn = cm[0][0]
@@ -152,6 +164,7 @@ print("Accuracy : ",accuracy*100)
 print("\n")
 
 ###################### XG Boost############################
+#       https://xgboost.readthedocs.io/en/stable/python/python_api.html?highlight=train#xgboost.train
 model = XGBClassifier()
 model.fit(X_train, Y_train)
 #make predictions for test data
@@ -178,8 +191,11 @@ print("Accuracy : ",accuracy*100)
 print("\n")
 
 ###################### Naive Bayes ########################
+#       https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html
 gnb = GaussianNB()
+#	Fit Gaussian Naive Bayes according to X, y.
 gnb.fit(X_train, Y_train)
+#	 Perform classification on an array of test vectors X.
 y_pred = gnb.predict(X_test)
 cm = confusion_matrix(Y_test,y_pred)
 tn = cm[0][0]
